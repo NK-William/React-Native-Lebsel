26f8cf4634ed49cd9bf35bbd370e7a89249cc879
Nkuna William <NkunaTebogo09@gmail.com>
2021-12-30 03:53:59 +0200

    Added components on a login screen

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import LoginScreen from "./src/screens/LoginScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <SafeAreaProvider>
+      <LoginScreen />
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import { StyleSheet, View, TextInput, Button } from "react-native";
+import SafeAreaView from "react-native-safe-area-view";
+import { Text } from "react-native-elements";
 
 const LoginScreen = () => {
   return (
-    <SafeAreaView style={styles.container} forceInset={{ top: "always" }}>
-      <View style={styles.top_sub_container}></View>
-      <View style={styles.middle_sub_container}></View>
-      <View style={styles.bottom_sub_container}></View>
+    <SafeAreaView
+      style={styles.safeAreaContainer}
+      forceInset={{ top: "always" }}
+    >
+      <View style={styles.container}>
+        <View style={styles.top_space}></View>
+        <View style={styles.sub_container}>
+          <Text style={styles.headerText} h1>
+            Login
+          </Text>
+          <Text style={styles.text}>Please sign in to continue</Text>
+          <Text style={styles.emailText}>EMAIL</Text>
+          <TextInput style={styles.textInput} textAlign={"center"} />
+          <Text style={styles.passwordText}>PASSWORD</Text>
+          <TextInput style={styles.textInput} textAlign={"center"} />
+          <Button
+            style={styles.submitButton}
+            title="LOGIN"
+            onPress={() => {}}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -14,25 +34,47 @@ const LoginScreen = () => {
 export default LoginScreen;
 
 const styles = StyleSheet.create({
+  headerText: { color: "white" },
+  text: { color: "#615E67", marginTop: 8, fontSize: 15 },
+  emailText: {
+    marginTop: 50,
+    marginLeft: 8,
+    marginBottom: 8,
+    color: "#615E67",
+    fontSize: 12,
+  },
+  passwordText: {
+    marginTop: 16,
+    marginLeft: 8,
+    marginBottom: 8,
+    color: "#615E67",
+    fontSize: 12,
+  },
+  textInput: {
+    borderColor: "#0DF6E3",
+    borderWidth: 1,
+    color: "white",
+    alignItems: "center",
+    padding: 8,
+    borderRadius: 12,
+  },
+  submitButton: {
+    backgroundColor: "#0DF6E3",
+    // height: 60,
+    // width: 200,
+    // marginTop: 50,
+    // borderRadius: 50,
+  },
+  safeAreaContainer: { flex: 1 },
   container: {
     flex: 1,
-    borderColor: "green",
-    backgroundColor: "#1F1A30",
-    borderWidth: 5,
+    padding: 24,
+    backgroundColor: "#201C31",
   },
-  top_sub_container: {
+  top_space: {
     flex: 0.1,
-    borderColor: "yellow",
-    borderWidth: 5,
   },
-  middle_sub_container: {
-    flex: 0.2,
-    borderColor: "red",
-    borderWidth: 5,
-  },
-  bottom_sub_container: {
-    flex: 0.7,
-    borderColor: "blue",
-    borderWidth: 5,
+  sub_container: {
+    flex: 0.9,
   },
 });
