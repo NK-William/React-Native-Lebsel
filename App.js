8e554c6d3b62c9d9f1aa29bdd248fbc906a364d7
Nkuna William <NkunaTebogo09@gmail.com>
2022-01-06 00:03:52 +0200

    Added register screen and all the components

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <LoginScreen />
+      <RegisterScreen />
     </SafeAreaProvider>
   );
 }
---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,138 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Text } from "react-native-elements";
+
+const RegisterScreen = () => {
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.headerText} h1>
+          Create Account
+        </Text>
+        <Text style={styles.text}>Please fill all the fields to sign up</Text>
+        <Text style={styles.topHintText}>NAME</Text>
+        <TextInput
+          style={styles.textInput}
+          textAlign={"center"}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text style={styles.hintText}>SURNAME</Text>
+        <TextInput
+          style={styles.textInput}
+          textAlign={"center"}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text style={styles.hintText}>EMAIL</Text>
+        <TextInput
+          style={styles.textInput}
+          textAlign={"center"}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text style={styles.hintText}>PASSWORD</Text>
+        <TextInput
+          style={styles.textInput}
+          textAlign={"center"}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text style={styles.hintText}>RE-ENTER PASSWORD</Text>
+        <TextInput
+          style={styles.textInput}
+          textAlign={"center"}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity style={styles.submitButton} onPress={() => {}}>
+          <Text style={styles.SubmitButtonText}>SUBMIT</Text>
+        </TouchableOpacity>
+        <View style={styles.bottom_container}>
+          <View style={styles.signUpLinkContainer}>
+            <Text style={styles.text}>Already have an account? </Text>
+            <TouchableOpacity onPress={() => {}}>
+              <Text style={styles.linkText}>Sign in</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Text>testing</Text>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    paddingRight: 12,
+    paddingTop: 64,
+    backgroundColor: "#201C31",
+  },
+  scrollView: {
+    paddingRight: 12,
+  },
+  headerText: { color: "white" },
+  text: { color: "#615E67", marginTop: 16, fontSize: 15 },
+  topHintText: {
+    marginTop: 50,
+    marginLeft: 8,
+    marginBottom: 8,
+    color: "#615E67",
+    fontSize: 12,
+  },
+  hintText: {
+    marginTop: 16,
+    marginLeft: 8,
+    marginBottom: 8,
+    color: "#615E67",
+    fontSize: 12,
+  },
+  textInput: {
+    borderColor: "#0DF6E3",
+    borderWidth: 1,
+    color: "white",
+    alignItems: "center",
+    padding: 8,
+    borderRadius: 12,
+  },
+  submitButton: {
+    backgroundColor: "#0DF6E3",
+    height: 60,
+    width: 200,
+    marginTop: 50,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  SubmitButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#201C31",
+  },
+  bottom_container: {
+    flex: 0.1,
+  },
+  signUpLinkContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  linkText: {
+    color: "#0DF6E3",
+    marginTop: 16,
+    fontSize: 15,
+    alignSelf: "center",
+  },
+});
