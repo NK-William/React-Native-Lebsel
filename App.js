2b162441f0970ed806218902c64c74079786e5f7
Nkuna William <NkunaTebogo09@gmail.com>
2022-01-06 00:08:16 +0200

    Added a blank home screen

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <RegisterScreen />
+      <LoginScreen />
     </SafeAreaProvider>
   );
 }
