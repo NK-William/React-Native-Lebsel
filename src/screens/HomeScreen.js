297c711a27551e0cd7049c59b027935756b26bf5
Nkuna William <NkunaTebogo09@gmail.com>
2022-01-11 12:57:05 +0200

    Added basic carousel view

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,48 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Dimensions,
+  Pressable,
+} from "react-native";
 import { auth } from "../firebase-config";
 import { signOut } from "firebase/auth";
+import Carousel from "react-native-snap-carousel";
+
+let FirstItem = 3;
+// FirstItem = 20;  // <----- UNCOMMENT THIS
+
+const SliderWidth = Dimensions.get("screen").width;
+const ItemWidth = 250.0;
+const ItemHeight = 400.0;
+
+const NumItems = 5;
+const Items = [];
+for (var i = 0; i < NumItems; i++) {
+  Items.push(i);
+}
 
 const HomeScreen = ({ navigation }) => {
+  const renderItem = ({ item }) => {
+    return (
+      <Pressable onPress={() => console.log(item)}>
+        <View
+          style={{
+            width: ItemWidth,
+            height: ItemHeight,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "blue",
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 40 }}>{item}</Text>
+        </View>
+      </Pressable>
+    );
+  };
+
   const handleLogoutTest = async () => {
     try {
       await signOut(auth);
@@ -14,8 +53,16 @@ const HomeScreen = ({ navigation }) => {
   };
   return (
     <View style={styles.container}>
-      <Text>Home page</Text>
       <Button onPress={handleLogoutTest} title="logout test" />
+      <Carousel
+        layout={"stack"}
+        data={Items}
+        firstItem={FirstItem}
+        itemWidth={ItemWidth}
+        sliderWidth={SliderWidth}
+        activeSlideAlignment="center"
+        renderItem={renderItem}
+      />
     </View>
   );
 };
@@ -27,6 +74,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "yellow",
   },
 });
