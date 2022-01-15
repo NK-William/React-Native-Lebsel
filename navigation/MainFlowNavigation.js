523572e9b21d74d2258c2ce02c998eb9a26d47e5
Nkuna William <NkunaTebogo09@gmail.com>
2022-01-15 16:46:23 +0200

    Added botton tab navigation icons

--- navigation/MainFlowNavigation.js
+++ navigation/MainFlowNavigation.js
@@ -9,23 +9,45 @@ import TimeSheetsScreen from "../src/screens/TimeSheetsScreen";
 import UniformRecordsScreen from "../src/screens/UniformRecordsScreen";
 import PromotersScreen from "../src/screens/PromotersScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
+const tabItemColor = "#201C31";
+
 function BottomTabNav() {
   return (
     <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      />
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
           headerStyle: { backgroundColor: "#201C31" },
           headerTitleStyle: { color: "white" },
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
         }}
       />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
