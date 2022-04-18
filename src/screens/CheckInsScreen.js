cd3fe7dd1829ecc7854ff3de944622a944e5513c
Nkuna William <NkunaTebogo09@gmail.com>
2022-04-18 23:28:46 +0200

    Added location on the map

--- src/screens/CheckInsScreen.js
+++ src/screens/CheckInsScreen.js
@@ -1,25 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native'
-import MapView from 'react-native-maps';
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
+import React, { useState, useEffect } from "react";
+import * as Location from "expo-location";
 
 // current state: https://docs.expo.dev/versions/latest/sdk/location/#configuration (I already installed expo-location)
 const CheckInsScreen = () => {
-    return (
-        <View style={styles.container}>
-            <MapView style={styles.map}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }} />
-        </View>
-    )
-}
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-export default CheckInsScreen
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  alert(text);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={
+          location
+            ? {
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }
+            : null
+        }
+      />
+    </View>
+  );
+};
+
+export default CheckInsScreen;
 
 const styles = StyleSheet.create({
-    container: { flex: 1 },
-    map: { width: "100%", height: "100%" }
-})
+  container: { flex: 1 },
+  map: { width: "100%", height: "100%" },
+});
