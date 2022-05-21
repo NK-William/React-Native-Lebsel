9283b2567eec5480e0134bd38d8cb3890ff91fd7
Nkuna William <NkunaTebogo09@gmail.com>
2022-05-21 22:49:23 +0200

    Added location marker

--- src/screens/CheckInsScreen.js
+++ src/screens/CheckInsScreen.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import React, { useState, useEffect } from "react";
 import * as Location from "expo-location";
+import { PrimaryColor } from "../styles/Colors";
 
 // current state: https://docs.expo.dev/versions/latest/sdk/location/#configuration (I already installed expo-location)
 const CheckInsScreen = () => {
@@ -22,28 +23,34 @@ const CheckInsScreen = () => {
     })();
   }, []);
 
-  let text = "Waiting..";
+  // let text = "Waiting...";
   if (errorMsg) {
     text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
+    alert(text);
   }
-
-  //   alert(text);
+  //  else if (location) {
+  //   text = JSON.stringify(location);
+  // }
 
   const mapRegion = location
     ? {
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitudeDelta: 0.0122,
+        longitudeDelta: 0.0121,
       }
     : null;
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} region={mapRegion} />
-      {location ? <Marker coordinate={mapRegion} title="Marker" /> : null}
+      <MapView style={styles.map} region={mapRegion}>
+        {location ? (
+          <Marker coordinate={mapRegion} title="Your location" />
+        ) : null}
+      </MapView>
+      <View style={styles.checkView}>
+        <Text style={styles.checkText}>Check in</Text>
+      </View>
     </View>
   );
 };
@@ -53,4 +60,17 @@ export default CheckInsScreen;
 const styles = StyleSheet.create({
   container: { flex: 1 },
   map: { width: "100%", height: "100%" },
+  checkView: {
+    backgroundColor: PrimaryColor,
+    position: "absolute",
+    bottom: 8,
+    padding: 10,
+    width: "90%",
+    alignSelf: "center",
+    borderRadius: 8,
+  },
+  checkText: {
+    color: "white",
+    alignSelf: "center",
+  },
 });
