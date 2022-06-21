ffc0c695892e783f817054b8794df9be55784e9a
Nkuna William <NkunaTebogo09@gmail.com>
2022-06-21 23:14:08 +0200

    Added profile page contents

--- src/screens/EmployeeProfileScreen.js
+++ src/screens/EmployeeProfileScreen.js
@@ -1,11 +1,68 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View, Image, Text } from "react-native";
 import React from "react";
-import { LighterPrimaryColor } from "../styles/Colors";
+import { LighterPrimaryColor, PrimaryColor } from "../styles/Colors";
+import { FontAwesome5, FontAwesome } from "@expo/vector-icons";
 
+// Add Glassmorphism and line dividers between texts.
 const EmployeeProfileScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.cardView}></View>
+      <View style={styles.cardView}>
+        <Image
+          style={styles.image}
+          source={require("../../public/images/pp.jpg")}
+        />
+        <View style={styles.floatingButtonContainer}>
+          <TouchableOpacity style={styles.floatingButton}>
+            <FontAwesome5 name="pen" size={24} color={PrimaryColor} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.floatingButton}>
+            <FontAwesome name="camera" size={24} color={PrimaryColor} />
+          </TouchableOpacity>
+        </View>
+        <Text
+          style={{
+            color: PrimaryColor,
+            marginBottom: 8,
+            marginTop: 4,
+            fontSize: 20,
+            marginHorizontal: 20,
+            fontWeight: "bold",
+          }}
+        >
+          William Nkuna
+        </Text>
+        <Text
+          style={{
+            color: PrimaryColor,
+            marginBottom: 4,
+            fontSize: 16,
+            marginHorizontal: 20,
+          }}
+        >
+          William@gmail.com
+        </Text>
+        <Text
+          style={{
+            color: PrimaryColor,
+            marginBottom: 4,
+            fontSize: 16,
+            marginHorizontal: 20,
+          }}
+        >
+          Centurion, Pretoria, Village Valencia
+        </Text>
+        <Text
+          style={{
+            color: PrimaryColor,
+            marginBottom: 4,
+            fontSize: 16,
+            marginHorizontal: 20,
+          }}
+        >
+          0712345678
+        </Text>
+      </View>
     </View>
   );
 };
@@ -20,5 +77,20 @@ const styles = StyleSheet.create({
     margin: 12,
     marginTop: 24,
     borderRadius: 8,
+  },
+  image: {
+    marginTop: 40,
+    alignSelf: "center",
+    width: 300,
+    borderRadius: 8,
+    height: 300,
+  },
+  floatingButtonContainer: {
+    marginTop: 8,
+    alignSelf: "flex-end",
+    flexDirection: "row",
+  },
+  floatingButton: {
+    marginRight: 18,
   },
 });
