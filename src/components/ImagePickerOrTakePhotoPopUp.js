0621509b2586a057011e04aba50840b79f2dc4e9
Nkuna William <NkunaTebogo09@gmail.com>
2022-07-07 10:04:34 +0200

    Added shadows

--- src/components/ImagePickerOrTakePhotoPopUp.js
+++ src/components/ImagePickerOrTakePhotoPopUp.js
@@ -117,5 +117,12 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     justifyContent: "center",
     alignItems: "center",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
   },
 });
