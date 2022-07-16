bcfb057469120afc759be554aef137e69da7732d
Nkuna William <NkunaTebogo09@gmail.com>
2022-07-16 23:13:25 +0200

    Moved some code to the component

--- src/screens/EmployeeAvailabilityScreen.js
+++ src/screens/EmployeeAvailabilityScreen.js
@@ -1,8 +1,6 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
-import { PrimaryColor, AccentColor } from "../styles/Colors";
-import { LinearGradient } from "expo-linear-gradient";
-import { Ionicons } from "@expo/vector-icons";
+import { PrimaryColor } from "../styles/Colors";
 import DayBubble from "../components/DayBubble";
 
 const EmployeeAvailabilityScreen = () => {
@@ -45,7 +43,7 @@ const EmployeeAvailabilityScreen = () => {
         <View style={styles.gridRowView}>
           <DayBubble
             bubbleRightColor="yellow"
-            day="Moondayy"
+            day="Monday"
             dayRadioBtnName={mondayRadioBtnName}
             setRadioButtons={() =>
               setRadioButtons({
@@ -54,163 +52,78 @@ const EmployeeAvailabilityScreen = () => {
               })
             }
           />
-
-          <LinearGradient
-            style={{ ...styles.dayBubble, marginLeft: 8 }}
-            colors={[PrimaryColor, "pink"]}
-            start={{ x: 0.4, y: 0.4 }}
-            end={{ x: 1, y: 1 }}
-          >
-            <Text style={styles.dayText}>Tuesday</Text>
-            <View style={styles.weeklySelectView}>
-              <Text style={styles.weeklyText}>Weekly</Text>
-              <TouchableOpacity
-                onPress={() =>
-                  setRadioButtons({
-                    ...radioButtons,
-                    tuesdayWeekly: !radioButtons.tuesdayWeekly,
-                  })
-                }
-              >
-                <Ionicons
-                  name={tuesdayRadioBtnName}
-                  size={16}
-                  color={AccentColor}
-                />
-              </TouchableOpacity>
-            </View>
-          </LinearGradient>
+          <DayBubble
+            bubbleRightColor="pink"
+            day="Tuesday"
+            dayRadioBtnName={tuesdayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                tuesdayWeekly: !radioButtons.tuesdayWeekly,
+              })
+            }
+          />
         </View>
         <View style={styles.gridRowView}>
-          <LinearGradient
-            style={{ ...styles.dayBubble, marginRight: 8 }}
-            colors={[PrimaryColor, "green"]}
-            start={{ x: 0.4, y: 0.4 }}
-            end={{ x: 1, y: 1 }}
-          >
-            <Text style={styles.dayText}>Wednesday</Text>
-            <View style={styles.weeklySelectView}>
-              <Text style={styles.weeklyText}>Weekly</Text>
-              <TouchableOpacity
-                onPress={() =>
-                  setRadioButtons({
-                    ...radioButtons,
-                    wednesdayWeekly: !radioButtons.wednesdayWeekly,
-                  })
-                }
-              >
-                <Ionicons
-                  name={wednesdayRadioBtnName}
-                  size={16}
-                  color={AccentColor}
-                />
-              </TouchableOpacity>
-            </View>
-          </LinearGradient>
-          <LinearGradient
-            style={{ ...styles.dayBubble, marginLeft: 8 }}
-            colors={[PrimaryColor, "orange"]}
-            start={{ x: 0.4, y: 0.4 }}
-            end={{ x: 1, y: 1 }}
-          >
-            <Text style={styles.dayText}>Thursday</Text>
-            <View style={styles.weeklySelectView}>
-              <Text style={styles.weeklyText}>Weekly</Text>
-              <TouchableOpacity
-                onPress={() =>
-                  setRadioButtons({
-                    ...radioButtons,
-                    thursdayWeekly: !radioButtons.thursdayWeekly,
-                  })
-                }
-              >
-                <Ionicons
-                  name={thursdayRadioBtnName}
-                  size={16}
-                  color={AccentColor}
-                />
-              </TouchableOpacity>
-            </View>
-          </LinearGradient>
+          <DayBubble
+            bubbleRightColor="green"
+            day="Wednesday"
+            dayRadioBtnName={wednesdayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                wednesdayWeekly: !radioButtons.wednesdayWeekly,
+              })
+            }
+          />
+          <DayBubble
+            bubbleRightColor="orange"
+            day="Thursday"
+            dayRadioBtnName={thursdayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                thursdayWeekly: !radioButtons.thursdayWeekly,
+              })
+            }
+          />
         </View>
         <View style={styles.gridRowView}>
-          <LinearGradient
-            style={{ ...styles.dayBubble, marginRight: 8 }}
-            colors={[PrimaryColor, "blue"]}
-            start={{ x: 0.4, y: 0.4 }}
-            end={{ x: 1, y: 1 }}
-          >
-            <Text style={styles.dayText}>Friday</Text>
-            <View style={styles.weeklySelectView}>
-              <Text style={styles.weeklyText}>Weekly</Text>
-              <TouchableOpacity
-                onPress={() =>
-                  setRadioButtons({
-                    ...radioButtons,
-                    fridayWeekly: !radioButtons.fridayWeekly,
-                  })
-                }
-              >
-                <Ionicons
-                  name={fridayRadioBtnName}
-                  size={16}
-                  color={AccentColor}
-                />
-              </TouchableOpacity>
-            </View>
-          </LinearGradient>
-          <LinearGradient
-            style={{ ...styles.dayBubble, marginLeft: 8 }}
-            colors={[PrimaryColor, "purple"]}
-            start={{ x: 0.4, y: 0.4 }}
-            end={{ x: 1, y: 1 }}
-          >
-            <Text style={styles.dayText}>Saturday</Text>
-            <View style={styles.weeklySelectView}>
-              <Text style={styles.weeklyText}>Weekly</Text>
-              <TouchableOpacity
-                onPress={() =>
-                  setRadioButtons({
-                    ...radioButtons,
-                    saturdayWeekly: !radioButtons.saturdayWeekly,
-                  })
-                }
-              >
-                <Ionicons
-                  name={saturdayRadioBtnName}
-                  size={16}
-                  color={AccentColor}
-                />
-              </TouchableOpacity>
-            </View>
-          </LinearGradient>
+          <DayBubble
+            bubbleRightColor="blue"
+            day="Friday"
+            dayRadioBtnName={fridayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                fridayWeekly: !radioButtons.fridayWeekly,
+              })
+            }
+          />
+          <DayBubble
+            bubbleRightColor="purple"
+            day="Saturday"
+            dayRadioBtnName={saturdayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                saturdayWeekly: !radioButtons.saturdayWeekly,
+              })
+            }
+          />
         </View>
         <View style={styles.gridRowView}>
-          <LinearGradient
-            style={{ ...styles.dayBubble, marginBottom: 18 }}
-            colors={[PrimaryColor, "red"]}
-            start={{ x: 0.4, y: 0.4 }}
-            end={{ x: 1, y: 1 }}
-          >
-            <Text style={styles.dayText}>Sunday</Text>
-            <View style={styles.weeklySelectView}>
-              <Text style={styles.weeklyText}>Weekly</Text>
-              <TouchableOpacity
-                onPress={() =>
-                  setRadioButtons({
-                    ...radioButtons,
-                    sundayWeekly: !radioButtons.sundayWeekly,
-                  })
-                }
-              >
-                <Ionicons
-                  name={sundayRadioBtnName}
-                  size={16}
-                  color={AccentColor}
-                />
-              </TouchableOpacity>
-            </View>
-          </LinearGradient>
+          <DayBubble
+            bubbleRightColor="red"
+            day="Sunday"
+            dayRadioBtnName={sundayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                sundayWeekly: !radioButtons.sundayWeekly,
+              })
+            }
+          />
         </View>
       </View>
     </View>
@@ -227,23 +140,10 @@ const styles = StyleSheet.create({
     margin: 8,
     fontSize: 16,
   },
-  dayText: { color: "white", fontSize: 18 },
   container: { flex: 1 },
   gridRowView: {
     flexDirection: "row",
     marginVertical: 8,
-  },
-  weeklySelectView: {
-    flexDirection: "row",
-    position: "absolute",
-    bottom: 0,
-    alignItems: "center",
-    marginBottom: 4,
-  },
-  weeklyText: {
-    color: "white",
-    fontSize: 10,
-    marginRight: 2,
   },
   radioButton: {},
   gridView: {
@@ -251,19 +151,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     flex: 1,
-  },
-  dayBubble: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: 100,
-    width: 150,
-    borderRadius: 50,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
   },
 });
