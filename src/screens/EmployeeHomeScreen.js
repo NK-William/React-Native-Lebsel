0b254811023e2f70d6cd3721317eac9ed865508d
Nkuna William <NkunaTebogo09@gmail.com>
2022-07-23 18:43:14 +0200

    Minor change

--- src/screens/EmployeeHomeScreen.js
+++ src/screens/EmployeeHomeScreen.js
@@ -1,7 +1,5 @@
 import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
-import { auth } from "../firebase-config";
-import { signOut } from "firebase/auth";
 import { homeStyles } from "../styles/HomeStyles";
 import { PrimaryColor, AccentColor } from "../styles/Colors";
 import { Ionicons } from "@expo/vector-icons";
@@ -10,21 +8,12 @@ import { TabOptions } from "../constants/TabOptions";
 import EmployeeTimesheetsScreen from "./EmployeeTimesheetsScreen";
 
 const EmployeeHomeScreen = () => {
-  const handleLogoutTest = async () => {
-    try {
-      await signOut(auth);
-    } catch (error) {
-      alert(error.message);
-    }
-  };
-
   return (
     <View style={homeStyles.container}>
       <View style={homeStyles.subTopContainer}>
         <Text style={homeStyles.welcomeText}>Welcome</Text>
         <Text style={homeStyles.nameText}>Nkuna</Text>
         <Text style={homeStyles.nameText}>William</Text>
-        <Button onPress={handleLogoutTest} title="logout test" />
       </View>
       <View
         style={{
--- src/screens/EmployeeSettingsScreen.js
+++ src/screens/EmployeeSettingsScreen.js
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
+import { auth } from "../firebase-config";
+import { signOut } from "firebase/auth";
 
 const EmployeeSettingsScreen = () => {
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>EmployeeSettingsScreen</Text>
+      <View></View>
+      <Pressable style={styles.logoutButton} onPress={logout}>
+        <Text style={styles.logoutText}>Logout</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default EmployeeSettingsScreen;
 
-const styles = StyleSheet.create({ container: { flex: 1 } });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingBottom: 24,
+  },
+  logoutButton: {
+    alignSelf: "flex-start",
+    backgroundColor: "gray",
+    padding: 8,
+    borderRadius: 8,
+    marginLeft: 8,
+  },
+  logoutText: { color: "white" },
+});
