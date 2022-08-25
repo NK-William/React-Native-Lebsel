0588a6a7d2e5a5da53bd72de923c0a6fcb148caa
Nkuna William <NkunaTebogo09@gmail.com>
2022-08-25 10:29:22 +0200

    Minor change

--- navigation/MainFlowNavigation.js
+++ navigation/MainFlowNavigation.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import EmployeeFlowNavigation from "./EmployeeFlowNavigation";
 import { getDatabase, ref, child, get } from "firebase/database";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import SplashScreen from "../src/screens/SplashScreen";
+import EnterInAppErrorScreen from "../src/screens/EnterInAppErrorScreen";
 import AdminFlowNavigation from "./AdminFlowNavigation";
 
 const Stack = createNativeStackNavigator();
@@ -10,34 +12,54 @@ const Stack = createNativeStackNavigator();
 const MainFlowNavigation = ({ user }) => {
   const [determinePage, setDeterminePage] = useState({
     isAdmin: false,
+    isEmployee: false,
     pageInitialized: false,
+    isError: false,
   });
 
-  if (!determinePage.pageInitialized) {
-    console.log("get database");
-    const dbRef = ref(getDatabase());
-    console.log("Query");
-    get(child(dbRef, `users/admins/${user.uid}`))
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          setDeterminePage({
-            ...determinePage,
-            isAdmin: true,
-            pageInitialized: true,
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      if (!determinePage.pageInitialized) {
+        console.log("get database");
+        const dbRef = ref(getDatabase());
+        console.log("Query");
+        get(child(dbRef, `users/admins/${user.uid}`))
+          .then((snapshot) => {
+            if (snapshot.exists()) {
+              setDeterminePage({
+                ...determinePage,
+                isAdmin: true,
+                isEmployee: false,
+                pageInitialized: true,
+                isError: false,
+              });
+            } else {
+              setDeterminePage({
+                ...determinePage,
+                isAdmin: false,
+                isEmployee: true,
+                pageInitialized: true,
+                isError: false,
+              });
+            }
+          })
+          .catch((error) => {
+            console.error("********************************* ", error);
+            //alert("Opps...Something went wrong!");
+            setDeterminePage({
+              ...determinePage,
+              isAdmin: false,
+              isEmployee: false,
+              pageInitialized: true,
+              isError: true,
+            });
           });
-        } else {
-          setDeterminePage({
-            ...determinePage,
-            isAdmin: false,
-            pageInitialized: true,
-          });
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        alert("Something went wrong!!");
-      });
-  }
+      }
+    })();
+  }, [determinePage]);
+
   if (!determinePage.isAdmin && !determinePage.pageInitialized) {
     return (
       <Stack.Navigator initialRouteName="splash">
@@ -48,10 +70,18 @@ const MainFlowNavigation = ({ user }) => {
         />
       </Stack.Navigator>
     );
+  } else if (determinePage.isInitialized && determinePage.isError) {
+    console.log("******************************** in error statement");
   } else if (determinePage.isAdmin) {
+    console.log("****************** admin pages");
     return <AdminFlowNavigation />;
-  } else {
+  } else if (determinePage.isEmployee) {
+    console.log("****************** employee pages");
     return <EmployeeFlowNavigation />;
+  } else {
+    // display error page
+    console.log("****************** error page");
+    return <EnterInAppErrorScreen />;
   }
 };
 
---
+++ src/screens/EnterInAppErrorScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const EnterInAppErrorScreen = () => {
+  return (
+    <View>
+      <Text>EnterInAppErrorScreen</Text>
+    </View>
+  );
+};
+
+export default EnterInAppErrorScreen;
+
+const styles = StyleSheet.create({});
